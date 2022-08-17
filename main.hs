8905c4f8bd04dfa795b1397f59af9e891089b966
Daniel Nazarian <danieln@mercury.com>
2022-08-17 10:37:45 -0400

    added typeclass

--- main.hs
+++ main.hs
@@ -9,13 +9,22 @@ import Data.Pool
 import Database.Persist.Postgresql
 import Yesod
 
-newtype App = App { appConnectionPool :: Pool SqlBackend }
+
+newtype App = App { appConnectionPool :: Pool SqlBackend, appSettings :: AppSettings }
+
 
 mkYesod "App" [parseRoutes|
 / HomeR GET
 |]
 
+    
 instance Yesod App
+
+
+class Monad (YesodDB site) => YesodPersist site where
+    type YesodPersistBackend site
+    runDB :: YesodDB site a -> HandlerFor site a
+
 
 instance YesodPersist App where
   type YesodPersistBackend App = SqlBackend
@@ -23,10 +32,13 @@ instance YesodPersist App where
     pool <- getsYesod appConnectionPool
     runSqlPool action pool
 
+
 getHomeR :: Handler ()
 getHomeR = pure ()
 
+
 main :: IO ()
 main = runStderrLoggingT $ do
+  appSettings <- loadAppSettingsArgs [configSettingsYml] [] useEnv
   pool <- createPostgresqlPool "host=localhost port=5432 dbname=yesodapp" 10
-  lift $ warp 3000 $ App pool
+  lift $ warp 3000 $ App pool appSettings
