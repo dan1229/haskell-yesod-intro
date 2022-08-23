6ae53d69e724d742b82f46a31ba1d9e8384faa26
Daniel Nazarian <danieln@mercury.com>
2022-08-23 11:04:29 -0400

    cleaned up language extensions and user model

--- src/Models/User.hs
+++ src/Models/User.hs
@@ -1,16 +1,15 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE StandaloneDeriving #-}
-{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE TypeFamilies #-}
 module Models.User where
 
 import Database.Persist.TH
+import Data.Text
 
 share [mkPersist sqlSettings] [persistLowerCase|
+User sql=users
+  email Text
+  username Text
+  UniqueEmail email
+  UniqueUsername username
 |]
